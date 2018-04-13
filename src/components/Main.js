487ca64cbbe1b0f8ddfe0063e68c870278b81eb0
XSP16SCM97M <xma39@hawk.iit.edu>
2018-04-12 21:39:22 -0500

    stage2

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,29 +1,40 @@
 import React from 'react';
 import nba from 'nba';
-import { ShotChart } from './ShotChart';
-import { Profile } from "./Profile";
+import { Profile } from './Profile';
+import { DataViewContainer } from './DataViewContainer';
+import {SearchBar} from './SearchBar'
 
 export class Main extends React.Component {
     state = {
-        playerId: nba.findPlayer('Stephen Curry').playerId,
         playerInfo: {},
     }
 
+    handleSelectPlayer = (name) => {
+        this.loadPlayerInfo(name);
+    }
+
     componentDidMount() {
-        nba.stats.playerInfo({ PlayerID: this.state.playerId }).then((info) => {
+        this.loadPlayerInfo('Stephen Curry');
+    }
+
+    loadPlayerInfo = (playerName) => {
+        const id = nba.findPlayer(playerName).playerId;
+        nba.stats.playerInfo({ PlayerID: id }).then((info) => {
             const playerInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
             console.log(playerInfo);
             this.setState({
-                playerInfo: playerInfo,
+                playerInfo: playerInfo
             });
         });
     }
-
     render() {
         return (
             <div className="main">
-                <Profile playerId={this.state.playerId} playerInfo={this.state.playerInfo}/>
-                <ShotChart playerId={this.state.playerId}/>
+                <SearchBar handleSelectPlayer={this.handleSelectPlayer}/>
+                <div className="player">
+                    <Profile playerInfo={this.state.playerInfo}/>
+                    <DataViewContainer playerId={this.state.playerInfo.playerId}/>
+                </div>
             </div>
         );
     }
--- src/components/ShotChart.js
+++ src/components/ShotChart.js
@@ -9,10 +9,12 @@ window.d3_hexbin = {hexbin : hexbin}; // workaround library problem
 
 export class ShotChart extends React.Component {
     static propTypes = {
-        playerId: PropTypes.number.isRequired,
+        playerId: PropTypes.number,
+        minCount: PropTypes.number.isRequired,
     }
 
-    componentDidMount() {
+    componentDidUpdate() {
+        const _this = this;
         nba.stats.shots({
             PlayerID: this.props.playerId
         }).then((response) => {
@@ -25,8 +27,9 @@ export class ShotChart extends React.Component {
             }));
 
             const courtSelection = d3.select("#shot-chart");
+            courtSelection.html('');
             const chart_court = court().width(500);
-            const chart_shots = shots().shotRenderThreshold(2).displayToolTips(true).displayType("hexbin");
+            const chart_shots = shots().shotRenderThreshold(_this.props.minCount).displayToolTips(true).displayType("hexbin");
             courtSelection.call(chart_court);
             courtSelection.datum(final_shots).call(chart_shots);
         });
